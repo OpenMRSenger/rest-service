4913eb07a3ffea366e875b8385db2ede85012386
Ruubske <rubendekeizer@gmail.com>
2026-05-20 14:55:10 +0200

    Delete WebhookCredentials.java

--- src/main/java/openmrsenger/restservice/appointments/application/WebhookCredentials.java
+++
@@ -1,9 +0,0 @@
-package openmrsenger.restservice.appointments.application;
-
-public record WebhookCredentials(
-        String token,
-        String username,
-        String password,
-        String clientId,
-        String clientSecret
-) {}
